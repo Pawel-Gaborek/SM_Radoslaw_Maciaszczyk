21b7c08248d336a2cf6d0857728a68939c6cb3b0
Pawel-Gaborek <it@pawel.gaborek.com>
2021-11-30 18:45:19 +0100

    aa
    aa

---
+++ Lab3/AndroidTrivia-Starter_czesc_3/app/src/main/java/com/example/android/navigation/GameWonFragmentDirections.kt
@@ -0,0 +1,5 @@
+package com.example.android.navigation
+
+class GameWonFragmentDirections {
+
+}
---
+++ Lab3/czesc_4/AndroidTrivia-Starter_czesc_3/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,83 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+import androidx.navigation.findNavController
+
+class TitleFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+
+        binding.playButton.setOnClickListener{
+            binding.playButton.setOnClickListener { view : View ->
+                view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            }
+        }
+
+        setHasOptionsMenu(true)
+
+        Log.i("TitleFragment", "onCreateView called")
+
+
+        return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("TitleFragment", "onAttach called")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("TitleFragment", "onCreate called")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("TitleFragment", "onViewCreated called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("TitleFragment", "onStart called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("TitleFragment", "onResume called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("TitleFragment", "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("TitleFragment", "onStop called")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("TitleFragment", "onDestroyView called")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("TitleFragment", "onDetach called")
+}
