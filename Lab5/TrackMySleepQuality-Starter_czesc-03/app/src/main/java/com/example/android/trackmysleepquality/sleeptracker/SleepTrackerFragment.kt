bf84aedc9014d351f97236e327a0346266ea80f1
Pawel-Gaborek <it@pawel.gaborek.com>
2021-12-26 20:20:56 +0100

    Lab 5
    Lab 5

---
+++ Lab5/TrackMySleepQuality-Starter_czesc-03/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
+
+/**
+ * A fragment with buttons to record start and end times for sleep, which are saved in
+ * a database. Cumulative data is displayed in a simple scrollable TextView.
+ * (Because we have not learned about RecyclerView yet.)
+ */
+class SleepTrackerFragment : Fragment() {
+
+    /**
+     * Called when the Fragment is ready to display content to the screen.
+     *
+     * This function uses DataBindingUtil to inflate R.layout.fragment_sleep_quality.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Get a reference to the binding object and inflate the fragment views.
+        val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_sleep_tracker, container, false)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+        val sleepTrackerViewModel =
+            ViewModelProvider(
+                this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+        binding.setLifecycleOwner(this)
+
+
+        private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+
+        val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+        fun doneNavigating() {
+            _navigateToSleepTracker.value = null
+        }
+
+
+        fun onSetSleepQuality(quality: Int) {
+            viewModelScope.launch {
+                val tonight = database.get(sleepNightKey) ?: return@launch
+                tonight.sleepQuality = quality
+                database.update(tonight)
+
+
+                // Setting this state variable to true will alert the observer and trigger navigation.
+                _navigateToSleepTracker.value = true
+            }
+        }
+
+        sleepTrackerViewModel.showSnackBarEvent.observe(this, Observer {
+            if (it == true) { // Observed state is true.
+                Snackbar.make(
+                    requireActivity().findViewById(android.R.id.content),
+                    getString(R.string.cleared_message),
+                    Snackbar.LENGTH_SHORT // How long to display the message.
+                ).show()
+                sleepTrackerViewModel.doneShowingSnackbar()
+            }
+
+        })
+        return binding.root
+    }
+}
